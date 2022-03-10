c5f04acfa7fc5cc8601de0c688a5f98bbaca1750
Ahmad Fadel <ahfa@umich.edu>
2022-03-10 14:36:06 -0500

    Changed AddInteger.java

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,5 +1,13 @@
-class HelloWorld {
+public class AddIntegers {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int first = 10;
+        int second = 25;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
     }
 }
